63ddc246bd41ae7477456e29f86fa4872510db7c
SeongIkKim <kpic1638@gmail.com>
2021-06-18 17:08:16 +0900

    <fix> differenct data load in inference, train server

--- data/load_data_in_inference.py
+++ data/load_data_in_inference.py
@@ -3,7 +3,7 @@ import pandas as pd
 import boto3
 
 # load training data from s3
-file_name = '/opt/ml/serving/data/train_data.csv'
+file_name = '/root/serving/data/train_data.csv'
 bucket = 'boostcamp-dkt-data'
 key = 'train_dataset/train_data.csv'
 
@@ -17,7 +17,7 @@ client.download_file(bucket, key, file_name)
 
 
 # set df for additional data
-additional_data_path = "/opt/ml/serving/data/additional_data.csv"
+additional_data_path = "/root/serving/data/additional_data.csv"
 additional_df = pd.DataFrame([], columns=['userID', 'assessmentItemID', 'testId', 'answerCode', 'Timestamp', 'KnowledgeTag'])
 additional_df.to_csv(additional_data_path, index=False)
 
